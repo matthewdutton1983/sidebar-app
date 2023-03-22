4a446184c065934b7f3a0ce066bd00630b44e7d7
Matthew Dutton <matthewdutton1983@hotmail.co.uk>
2023-03-22 12:54:32 -0400

    Added modal to create template for labels

--- src/components/Annotation/Annotation.js
+++ src/components/Annotation/Annotation.js
@@ -1,15 +1,58 @@
 import { useState } from 'react';
-import { IconButton, Menu, Popover, ListItemText, ListItemIcon, Typography } from '@mui/material';
+import { IconButton, Menu, Popover, ListItemText, ListItemIcon, Typography, Modal, Button, Box, TextField, Chip } from '@mui/material';
 import { StyledListItem, ListItemIconWrapper, NoDataMessage, AnnotationText, StyledMenuItem } from '../StyledComponents';
 import { InfoRounded, MoreVertRounded, DeleteRounded } from '../IconImports';
 import './Annotation.styles.css';
+import { AddRounded } from '@mui/icons-material';
 
 export const AnnotationPanel = ({ annotations, onDeleteAnnotation }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [newTemplateName, setNewTemplateName] = useState('');
+  const [newTemplateLabels, setNewTemplateLabels] = useState([]);
+  const [newLabelValue, setNewLabelValue] = useState('');
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal =() => {
+    setIsModalOpen(false);
+    setNewTemplateName('');
+  };
+
+  const handleInputChange = (event) => {
+    setNewTemplateName(event.target.value);
+  };
+
+  const handleLabelInputChange = (event) => {
+    setNewLabelValue(event.target.value);
+  };
+
+  const handleAddNewLabel = () => {
+    if (newLabelValue.trim() !== '') {
+      setNewTemplateLabels([...newTemplateLabels, newLabelValue.trim()]);
+      setNewLabelValue('');
+    }
+  };
+
+  const handleDeleteLabel = (index) => {
+    const newLabels = [...newTemplateLabels];
+    newLabels.splice(index, 1);
+    setNewTemplateLabels(newLabels);
+  };
+
+  const handleCreateTemplate = () => {
+    console.log(`Creating new template: ${newTemplateName}`)
+  }
+
   return (
     <div className='annotations'>
       <Typography variant='h6' gutterBottom>
         Annotate Text
       </Typography>
+      <Button variant='contained' onClick={handleOpenModal} style={{ marginBottom: '15px' }}>
+        Create New Template
+      </Button>
       {annotations.length === 0 ? (
         <NoDataMessage>
           There are currently no annotations for this document.
@@ -26,6 +69,47 @@ export const AnnotationPanel = ({ annotations, onDeleteAnnotation }) => {
           />
         ))
       )}
+      <Modal open={isModalOpen} onClose={handleCloseModal}>
+          <div className='modal'>
+            <Typography variant='h6' gutterBottom>
+              Create New Template
+            </Typography>
+            <TextField 
+              placeholder='Please enter a name for this Annotation Template'
+              value={newTemplateName}
+              onChange={handleInputChange}
+              fullWidth
+              margin='normal'
+            />
+            <Box display='flex' alignItems='center'>
+              <TextField
+                placeholder='Add a new label and press Enter'
+                value={newLabelValue}
+                onChange={handleLabelInputChange}
+                margin='normal'
+                sx={{ flex: '1' }}
+                onKeyPress={(event) => {
+                  if (event.key === 'Enter') {
+                    handleAddNewLabel();
+                  }
+                }}
+              />
+            </Box>
+            <Box>
+              {newTemplateLabels.map((label, index) => (
+                <Chip key={index} label={label} onDelete={() => handleDeleteLabel(index)} style={{ margin: '4px' }} />
+              ))}
+            </Box>
+            <Box display='flex' justifyContent='flex-end' mt={2}>
+              <Button variant='contained' onClick={handleCloseModal} style={{ marginRight: '16px' }}>
+                Cancel
+              </Button>
+              <Button variant='contained' color='primary' onClick={handleCreateTemplate}>
+                Create
+              </Button>
+            </Box>
+          </div>
+      </Modal>
     </div>
   );
 };
