1bc06a3c3a3befad0f65c8b1143e92b3c5439cb2
Matthew Dutton <matthewdutton1983@hotmail.co.uk>
2023-03-24 08:28:44 -0400

    Starting again

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import { leftTools, rightTools } from './data/ToolsData';
-import { Toolbar } from './components/Toolbar/Toolbar';
-import { Document } from './components/Document/Document';
-import { useAnnotations } from './components/Annotator/AnnotationEntity/useAnnotationEntity';
-import { useComments } from './components/Comments/useComments';
-import './App.css';
+import { leftTools, rightTools } from "./data/ToolsData";
+import { Toolbar } from "./components/Toolbar/Toolbar";
+import { Document } from "./components/Document/Document";
+import { useComments } from "./components/Comments/useComments";
+import "./App.css";
 
 function App() {
-  const { annotations, handleAddAnnotation, handleDeleteAnnotation } = useAnnotations();
+  // const { annotations, handleAddAnnotation, handleDeleteAnnotation } =
+  //   useAnnotations();
   const { comments } = useComments();
 
   return (
-    <div className='container'>
-      <Toolbar 
-        tools={leftTools} 
-        position='left' 
-      />
-      <Document onAnnotate={handleAddAnnotation} />
-      <Toolbar 
-        tools={rightTools} 
-        position='right'
-        annotations={annotations} 
-        onDeleteAnnotation={handleDeleteAnnotation}
+    <div className="container">
+      <Toolbar tools={leftTools} position="left" />
+      {/* <Document onAnnotate={handleAddAnnotation} /> */}
+      <Document />
+      <Toolbar
+        tools={rightTools}
+        position="right"
+        // annotations={annotations}
+        // onDeleteAnnotation={handleDeleteAnnotation}
         comments={comments}
       />
     </div>
   );
-};
+}
 
 export default App;
--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,23 +1,23 @@
-import { useState } from 'react';
-import { AnnotationPanel } from '../Annotator/AnnotationPanel/AnnotationPanel';
-import { Classification } from '../Classifier/Classification';
-import { Comment } from '../Comments/Comments';
-import './Toolbar.styles.css';
+import { useState } from "react";
+// import { AnnotationPanel } from "../Annotator/AnnotationPanel/AnnotationPanel";
+import { Classification } from "../Classifier/Classification";
+import { Comment } from "../Comments/Comments";
+import "./Toolbar.styles.css";
 
-export const Toolbar = ({ 
-  tools, 
-  position, 
-  annotations, 
-  onDeleteAnnotation,
+export const Toolbar = ({
+  tools,
+  position,
+  // annotations,
+  // onDeleteAnnotation,
   comments,
 }) => {
   const [activeTool, setActiveTool] = useState(null);
   const [tags, setTags] = useState([]);
 
-    const handleToolClick = (tool) => {
+  const handleToolClick = (tool) => {
     setActiveTool(activeTool === tool ? null : tool);
   };
-  
+
   const handleAddTag = (tag) => {
     setTags([...tags, tag]);
   };
@@ -29,34 +29,40 @@ export const Toolbar = ({
   const renderPanelContent = () => {
     if (!activeTool) return null;
 
-    if (activeTool.name === 'Annotate') {
-      return (<AnnotationPanel annotations={annotations} onDeleteAnnotation={onDeleteAnnotation}/>);
-    };
-    
-    if (activeTool.name ==='Tags') {
-      return (<Classification tags={tags} onAddTag={handleAddTag} onDeleteTag={handleDeleteTag}/>);
-    };
+    // if (activeTool.name === 'Annotate') {
+    //   return (<AnnotationPanel annotations={annotations} onDeleteAnnotation={onDeleteAnnotation}/>);
+    // };
 
-    if (activeTool.name ==='Comments') {
-      return (<Comment comments={comments}/>);
-    };
-   
+    if (activeTool.name === "Tags") {
+      return (
+        <Classification
+          tags={tags}
+          onAddTag={handleAddTag}
+          onDeleteTag={handleDeleteTag}
+        />
+      );
+    }
+
+    if (activeTool.name === "Comments") {
+      return <Comment comments={comments} />;
+    }
+
     return activeTool.panel;
   };
 
   return (
-    <div className={ `toolbar ${position}` }>
+    <div className={`toolbar ${position}`}>
       {tools.map((tool) => (
         <div
           key={tool.id}
-          className={ `toolbar-item ${ activeTool === tool ? 'active' : '' }` }
+          className={`toolbar-item ${activeTool === tool ? "active" : ""}`}
           onClick={() => handleToolClick(tool)}
         >
           {tool.icon}
         </div>
       ))}
-      <div className={ `panel ${activeTool ? 'expanded' : ''} ${position}` }>
-        { renderPanelContent() }
+      <div className={`panel ${activeTool ? "expanded" : ""} ${position}`}>
+        {renderPanelContent()}
       </div>
     </div>
   );
