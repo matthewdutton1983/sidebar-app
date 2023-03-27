4d910b9fade680814abb90bee7f586da6465f53d
Matthew Dutton <matthewdutton1983@hotmail.co.uk>
2023-03-27 14:25:15 -0400

    moved add documents modal to a separate component

---
+++ src/components/Collection/AddDocumentsModal.js
@@ -0,0 +1,55 @@
+import { Modal, Typography, Button } from "@mui/material";
+import { useDropzone } from "react-dropzone";
+import { useCallback, useState } from "react";
+
+export const AddDocumentsModal = ({ open, onClose }) => {
+  const onDrop = useCallback((acceptedFiles) => {
+    // Do something with the files
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <Modal open={open} onClose={onClose} className="modal">
+      <div className="modal-content">
+        <div
+          {...getRootProps()}
+          className="dropzone"
+          style={{
+            border: "4px dashed #ddd",
+            borderRadius: "4px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            textAlign: "center",
+            justifyContent: "center",
+            fontSize: "24px",
+            fontWeight: "bold",
+            color: "#aaaaaa",
+            width: "1600px",
+            height: "800px",
+            transition: "0.3s",
+          }}
+        >
+          <input {...getInputProps()} />
+          {isDragActive ? (
+            <p>Drop the files here ...</p>
+          ) : (
+            <>
+              <Typography variant="h6" color="textPrimary">
+                Drag and drop files here
+              </Typography>
+              <Button
+                variant="outlined"
+                color="primary"
+                style={{ marginTop: "16px" }}
+              >
+                or click to select files
+              </Button>
+            </>
+          )}
+        </div>
+      </div>
+    </Modal>
+  );
+};
--- src/pages/CollectionPage.js
+++ src/pages/CollectionPage.js
@@ -1,19 +1,13 @@
 import { Link, useParams } from "react-router-dom";
-import { Button, Modal, Typography, IconButton } from "@mui/material";
-import { useCallback, useState } from "react";
-import { useDropzone } from "react-dropzone";
+import { Button, IconButton, Typography } from "@mui/material";
+import { useState } from "react";
 import { AddRounded, HomeRounded } from "../components/IconImports";
+import { AddDocumentsModal } from "../components/Collection/AddDocumentsModal";
 import { CollectionNotFound } from "../components/Collection/CollectionNotFound";
 import "../components/Collection/Collection.styles.css";
 
 export const CollectionPage = () => {
   const { collectionId } = useParams();
-
-  const onDrop = useCallback((acceptedFiles) => {
-    // Do something with the files
-  }, []);
-
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   const collections = JSON.parse(localStorage.getItem("collections")) || [];
   const collection = collections.find((c) => String(c.id) === collectionId);
@@ -43,10 +37,12 @@ export const CollectionPage = () => {
         <Typography variant="h5" sx={{ flexGrow: 1, paddingLeft: "16px" }}>
           {collection.name}
         </Typography>
-        <Button variant="contained" onClick={handleOpenModal}>
-          <AddRounded sx={{ paddingRight: "8px" }} />
-          Add Documents
-        </Button>
+        {collection.documents.length >= 1 && (
+          <Button variant="contained" onClick={handleOpenModal}>
+            <AddRounded sx={{ paddingRight: "8px" }} />
+            Add Documents
+          </Button>
+        )}
       </div>
       <div className="bottom-row">
         <div className="centered-container" style={{ textAlign: "center" }}>
@@ -71,47 +67,7 @@ export const CollectionPage = () => {
           ) : null}
         </div>
       </div>
-      <Modal open={isModalOpen} onClose={handleCloseModal} className="modal">
-        <div className="modal-content">
-          <div
-            {...getRootProps()}
-            className="dropzone"
-            style={{
-              border: "4px dashed #ddd",
-              borderRadius: "4px",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              textAlign: "center",
-              justifyContent: "center",
-              fontSize: "24px",
-              fontWeight: "bold",
-              color: "#aaaaaa",
-              width: "1600px",
-              height: "800px",
-              transition: "0.3s",
-            }}
-          >
-            <input {...getInputProps()} />
-            {isDragActive ? (
-              <p>Drop the files here ...</p>
-            ) : (
-              <>
-                <Typography variant="h6" color="textPrimary">
-                  Drag and drop files here
-                </Typography>
-                <Button
-                  variant="outlined"
-                  color="primary"
-                  style={{ marginTop: "16px" }}
-                >
-                  or click to select files
-                </Button>
-              </>
-            )}
-          </div>
-        </div>
-      </Modal>
+      <AddDocumentsModal open={isModalOpen} onClose={handleCloseModal} />
     </div>
   );
 };
