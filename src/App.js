e12ed925ef126938054a5ca36e61d5fd5769c64f
Matthew Dutton <matthewdutton1983@hotmail.co.uk>
2023-03-27 14:41:35 -0400

    Added CollectionNotFound message into CollectionPage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { CollectionPage } from "./pages/CollectionPage";
-import { CollectionNotFound } from "./components/Collection/CollectionNotFound";
 import { SmartViewerPage } from "./pages/SmartViewerPage";
 import "./App.css";
 
@@ -11,7 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/collection/:collectionId" element={<CollectionPage />} />
-        <Route path="/collection-not-found" element={<CollectionNotFound />} />
         <Route path="/smartviewer" element={<SmartViewerPage />} />
       </Routes>
     </BrowserRouter>
--- src/pages/CollectionPage.js
+++ src/pages/CollectionPage.js
@@ -3,7 +3,6 @@ import { Button, IconButton, Typography } from "@mui/material";
 import { useState } from "react";
 import { AddRounded, HomeRounded } from "../components/IconImports";
 import { AddDocumentsModal } from "../components/Modals/AddDocumentsModal";
-import { CollectionNotFound } from "../components/Collection/CollectionNotFound";
 import "../components/Collection/Collection.styles.css";
 
 export const CollectionPage = () => {
@@ -23,7 +22,20 @@ export const CollectionPage = () => {
   };
 
   if (!collection) {
-    return <CollectionNotFound />;
+    return (
+      <div className="collection-page">
+        <div className="top-row">
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <IconButton>
+              <HomeRounded sx={{ fontSize: "36px" }} />
+            </IconButton>
+          </Link>
+          <Typography variant="h5" sx={{ flexGrow: 1, paddingLeft: "16px" }}>
+            Collection not found
+          </Typography>
+        </div>
+      </div>
+    );
   }
 
   return (
